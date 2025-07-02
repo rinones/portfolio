4aabfe719128ae94b31f141dcde103d6e67a0732
rinones <rinonesdiez@gmail.com>
2025-07-02 20:51:54 +0200

    Remove unused assets, scripts, and commented code
    Deleted unused logo images, JavaScript files, and SCSS section files. Cleaned up index.html by removing commented-out sections and references to deleted scripts. This streamlines the project by eliminating obsolete resources and code.

--- js/img-links.js
+++
@@ -1,8 +0,0 @@
-// document.querySelectorAll('.job-float-img').forEach(img => {
-//   img.addEventListener('click', function () {
-//     const url = this.getAttribute('data-url');
-//     if (url) {
-//       window.open(url, '_blank');
-//     }
-//   });
-// });
